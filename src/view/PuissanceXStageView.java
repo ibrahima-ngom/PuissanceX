b84d75080161b2178d2bb9b1b5f8b80763c75071
Rimes Chekki <rimes.chekki@edu.univ-fcomte.fr>
2025-05-04 18:50:58 +0200

    implémentation de la partie view
    implémentation de la partie view

--- src/view/PuissanceXStageView.java
+++ src/view/PuissanceXStageView.java
@@ -1,4 +1,28 @@
 package view;
 
 public class PuissanceXStageView {
+    // Affiche la grille complète à partir d'une matrice de pions
+    public void afficherGrille(int[][] grille) {
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[i].length; j++) {
+                System.out.print(PuissanceXTokenLook.getLook(grille[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Affichage de  message de victoire
+    public void afficherMessageVictoire(int joueur) {
+        System.out.println("Victoire du joueur " + joueur + " !");
+    }
+
+    //  un message d'égalité
+    public void afficherMessageEgalite() {
+        System.out.println("Match nul !");
+    }
+
+    // Affichage d'un message générique
+    public void afficherMessage(String message) {
+        System.out.println(message);
+    }
 }
--- src/view/PuissanceXTokenLook.java
+++ src/view/PuissanceXTokenLook.java
@@ -1,4 +1,16 @@
 package view;
 
 public class PuissanceXTokenLook {
+    // Retourne le caractère à afficher selon la valeur du pion
+    // 0 :vide, 1 : rouge, 2 : jaune
+    public static char getLook(int pion) {
+        switch (pion) {
+            case 1:
+                return 'O'; // Rouge
+            case 2:
+                return 'X'; // Jaune
+            default:
+                return '.'; // Vide
+        }
+    }
 }
